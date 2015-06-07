b94a5c253d1240698d715722c430a65ec3d3825f
Richard Harrison <richard@richardharrison.es>
2015-06-07 21:46:07 +0200

    Update deprecated UserSettings API call.
    UserSettings is now deprecated in Piwik, use DevicesDetection.getOsFamilies instead.

--- classes/WP_Piwik/Widget/Systems.php
+++ classes/WP_Piwik/Widget/Systems.php
@@ -12,7 +12,7 @@
 				'date'  => $timeSettings['date']
 			);
 			$this->title = $prefix.__('Operation Systems', 'wp-piwik').' ('.__($timeSettings['description'],'wp-piwik').')';
-			$this->method = 'UserSettings.getOS';
+			$this->method = 'DevicesDetection.getOsFamilies';
 			$this->context = 'normal';
 			wp_enqueue_script('wp-piwik', self::$wpPiwik->getPluginURL().'js/wp-piwik.js', array(), self::$wpPiwik->getPluginVersion(), true);
 			wp_enqueue_script('wp-piwik-jqplot',self::$wpPiwik->getPluginURL().'js/jqplot/wp-piwik.jqplot.js',array('jquery'));
